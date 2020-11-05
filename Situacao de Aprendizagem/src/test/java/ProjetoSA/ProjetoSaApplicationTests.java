53c341c4cb97b4800648c6d21bcaae7b18635418
RafaelSantosOliveira <rsrafael2014@hotmail.com>
2020-11-05 13:24:42 -0300

    Inicialização do Projeto

---
+++ Situacao de Aprendizagem/src/test/java/ProjetoSA/ProjetoSaApplicationTests.java
@@ -0,0 +1,13 @@
+package ProjetoSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
